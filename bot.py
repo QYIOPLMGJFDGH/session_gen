51cb96292bbfaa24084a9118ae5ed738e49f77d8
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2024-11-19 11:44:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,9 +10,9 @@ from pyrogram import Client
 from pyromod import listen
 
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+API_ID = int(os.environ.get("API_ID", "16457832"))
+API_HASH = os.environ.get("API_HASH", "3030874d0befdb5d05597deacc3e83ab")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7532369778:AAFdUOmEFV1ASjid2Al1JwJz7dIoulyIi5Q")
 API_KEY = os.environ.get("API_KEY", None)
 
 
